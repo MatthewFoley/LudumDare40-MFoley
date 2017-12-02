ad540ca59066f6f4193654f2eafc78d8054f1ff9
MatthewFoley <mrfoley54@gmail.com>
2017-12-02 15:44:53 +0000

    Truck controller working with physics based system and virtual roll bar

---
+++ CrazyTrucker/Assets/Test Assets/RollBar.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollBar : MonoBehaviour {
+
+	public WheelCollider leftWheel;
+	public WheelCollider rightWheel;
+	private Rigidbody truckRigidBody;
+
+	public float AntiRoll = 5000.0f;
+
+	void Start ()
+	{
+		truckRigidBody = GetComponent <Rigidbody>();
+	}
+
+	void FixedUpdate ()
+	{
+		WheelHit hit = new WheelHit();
+		float travelL = 1.0f;
+		float travelR = 1.0f;
+
+		bool leftgrounded = leftWheel.GetGroundHit(out hit);
+
+		if (leftgrounded)
+		{
+			travelL = (-leftWheel.transform.InverseTransformPoint(hit.point).y 
+				- leftWheel.radius) / leftWheel.suspensionDistance;
+		}
+
+		bool rightgrounded = rightWheel.GetGroundHit(out hit);
+
+		if (rightgrounded)
+		{
+			travelR = (-rightWheel.transform.InverseTransformPoint(hit.point).y 
+				- rightWheel.radius) / rightWheel.suspensionDistance;
+		}
+
+		var antiRollForce = (travelL - travelR) * AntiRoll;
+
+		if (leftgrounded)
+			truckRigidBody.AddForceAtPosition(leftWheel.transform.up * -antiRollForce,
+				leftWheel.transform.position); 
+		if (rightgrounded)
+			truckRigidBody.AddForceAtPosition(rightWheel.transform.up * antiRollForce,
+				rightWheel.transform.position); 
+	}
+}
---
+++ CrazyTrucker/Assets/Test Assets/TruckController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TruckController : MonoBehaviour {
+
+	public WheelCollider[] wheelColliders = new WheelCollider[4];
+	public Transform[] wheelMeshes = new Transform[4];
+
+	public Transform resetPosition;
+	public Rigidbody truckMass;
+
+	public float maxTorque = 50f;
+	public float steering = 2f;
+
+	void Start()
+	{
+		//truckMass.centerOfMass += new Vector3 (0, 0, 1);
+	}
+
+	void Update()
+	{
+		UpdateWheels ();
+		if (Input.GetButtonDown ("Jump")) 
+		{
+			transform.position += new Vector3 (0, 2, 0);
+			transform.rotation = new Quaternion(0, 0, 0,0);
+
+			for (int i = 0; i < 4; i++) 
+			{
+				wheelColliders [i].motorTorque = 0;
+			}
+		}
+	}
+
+	void FixedUpdate()
+	{
+		float steer = Input.GetAxis ("Horizontal");
+		float accelerate = Input.GetAxis ("Vertical");
+
+		float finalSteer = steer * 45f;
+
+		wheelColliders [0].steerAngle = finalSteer;
+		wheelColliders [1].steerAngle = finalSteer;
+
+		for (int i = 2; i < 4; i++) 
+		{
+			wheelColliders [i].motorTorque = accelerate * maxTorque;
+		}
+	}
+
+	void UpdateWheels()
+	{
+		for (int i = 0; i < 4; i++) 
+		{
+			Quaternion quat;
+			Vector3 position;
+			wheelColliders[i].GetWorldPose (out position, out quat);
+
+			//wheelMeshes [i].position = position;
+			wheelMeshes [i].rotation = quat;
+			wheelMeshes [i].Rotate (Vector3.forward * (-90));
+			wheelMeshes [i].Rotate (Vector3.right * (180));
+		}
+	}
+}
