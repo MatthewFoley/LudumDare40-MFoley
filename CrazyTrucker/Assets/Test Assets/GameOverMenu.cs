db62eba0f4ee2357a4509df757fb489261875816
MatthewFoley <mrfoley54@gmail.com>
2017-12-03 23:31:42 +0000

    Menus completed

---
+++ CrazyTrucker/Assets/Test Assets/GameOverMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameOverMenu : MonoBehaviour {
+
+	public void Yes ()
+	{
+		SceneManager.LoadScene ("Level1");
+	}
+
+	public void No()
+	{
+		SceneManager.LoadScene ("MainMenu");
+	}
+
+
+}
