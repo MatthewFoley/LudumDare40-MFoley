f35c7fa9a445866d93e98b60a1c9d1dd24b34d11
MatthewFoley <mrfoley54@gmail.com>
2017-12-03 19:32:14 +0000

    WorldSpace UI added. Timer and score system added.

--- CrazyTrucker/Assets/Test Assets/DropOffPoint.cs
+++ CrazyTrucker/Assets/Test Assets/DropOffPoint.cs
@@ -6,13 +6,23 @@ public class DropOffPoint : MonoBehaviour {
 
 	public PackageCollector packages;
 	public Rigidbody truckMass;
+	public GameManager manager;
+	float  score = 0;
+	float timer;
 
 	void OnTriggerEnter(Collider col)
 	{
 		if (col.CompareTag ("Player")) 
 		{
-			packages.storedPackages = 0;
-			truckMass.mass = 400;
+			if (packages.storedPackages > 0) 
+			{
+				score = manager.playerScore + (packages.storedPackages * 10);
+				timer = manager.playerTimer + (packages.storedPackages * 30);
+				manager.playerTimer = timer;
+				manager.playerScore = score;
+				packages.storedPackages = 0;
+				truckMass.mass = 400;
+			}
 		}
 	}
 }
---
+++ CrazyTrucker/Assets/Test Assets/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+	public Text score;
+	public Text timer;
+	public Text crateCounter;
+	public float playerScore;
+	public float playerTimer;
+	public float crateCount;
+	public int tempTimer;
+	public PackageCollector package;
+
+	void Start()
+	{
+		playerTimer = 120;
+		playerScore = 0;
+	}
+
+	void Update()
+	{
+		tempTimer = (Mathf.RoundToInt(playerTimer));
+		crateCount = package.storedPackages;
+		playerTimer -= Time.deltaTime;
+		crateCounter.text = crateCount.ToString ();
+		timer.text = tempTimer.ToString ();
+		score.text = playerScore.ToString ();
+	}
+
+
+
+}
