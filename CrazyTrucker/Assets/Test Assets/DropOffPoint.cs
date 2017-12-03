112c93a6eca10cc64ab846bdd530d1922d992b4c
MatthewFoley <mrfoley54@gmail.com>
2017-12-03 17:26:15 +0000

    Level assembled with coliders

---
+++ CrazyTrucker/Assets/Test Assets/DropOffPoint.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropOffPoint : MonoBehaviour {
+
+	public PackageCollector packages;
+	public Rigidbody truckMass;
+
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.CompareTag ("Player")) 
+		{
+			packages.storedPackages = 0;
+			truckMass.mass = 400;
+		}
+	}
+}
